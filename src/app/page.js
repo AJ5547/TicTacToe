a2587eec9da84acef9682c144b9cb8516e509fc1
AJ5547 <alissa.jackson5547@stu.mtec.edu>
2023-11-01 21:40:42 -0600

    x's on board when click

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,36 @@
 "use client";
+import { useState } from "react";
 
-export default function Square() {
-  return <button className="square py-3"> X</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    setValue("X");
+  }
+  return (
+    <button onClick={handleClick} className="square">
+      {" "}
+      {value}
+    </button>
+  );
+}
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 }
